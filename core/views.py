fd1f332010499450820c1356f97d7c3eab59bd5d
thealiiheere <rustamovakbar291@gmail.com>
2026-09-13 17:01:17 +0500

    Delete core directory

--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def test(request):
-    course={"courses":["English", "Python", "Java", "CSS"]}
-    return render(request, 'core/index.html', course)
